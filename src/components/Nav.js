bbda513da7e69862e21bb4ca57a8b7d2f63cd2c2
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-13 08:58:22 -0700

    correction Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <nav className="main-nav"
+        <nav className="main-nav">
             <ul>
                 <li><NavLink to="/">Cats</NavLink></li>
                 <li><NavLink to="/">Dogs</NavLink></li>
                 <li><NavLink to="/">Computers</NavLink></li>
             </ul>
+        </nav>
     );
 }
 
