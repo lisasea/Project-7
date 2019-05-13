afd8a01ee97eebcb1fac91028d2bcf020379efb7
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-13 08:56:26 -0700

    beginning Nav.js to lead to Header.js

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <nav className="main-nav"
+            <ul>
+                <li><NavLink to="/">Cats</NavLink></li>
+                <li><NavLink to="/">Dogs</NavLink></li>
+                <li><NavLink to="/">Computers</NavLink></li>
+            </ul>
+    );
+}
+
+export default Nav;
