5ae0f0bd9ce969c9fda2adc09a402849bab1c0c9
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-15 08:34:26 -0700

    begin Search.js

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const GalleryItem = props => (  //display each list item and image.
     <li className="gif-wrap">
-        <img src={props.url}
-            alt="props.title" />
+        <img 
+            src={props.url}
+            alt={props.title} />
     </li>
 );
 
