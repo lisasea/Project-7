b3579ebeceebb889303d72784c0525be5d34630b
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-15 08:19:03 -0700

    GalleryItem

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,4 +1,10 @@
 import React from "react";
 
+const GalleryItem = props => (  //display each list item and image.
+    <li className="gif-wrap">
+        <img src={props.url}
+            alt="props.title" />
+    </li>
+);
 
 export default GalleryItem;
