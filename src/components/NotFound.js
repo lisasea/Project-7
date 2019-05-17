859fb54790634fe4028679b92ba9facf65a40600
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-17 08:25:40 -0700

    typo

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const NotFound = () +> {
+const NotFound = () => {
     return (
         <div className="not-found">
-            <h3>404-error Page Not found</h3>
+            <h3>404 Error - Page Not found</h3>
         </div>
     )
 }
